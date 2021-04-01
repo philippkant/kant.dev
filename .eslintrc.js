75216ab43f9acdf61264209206e650a6f3523687
Philipp Kant <ptckant@gmail.com>
2021-04-01 14:58:27 +0200

    initial react app

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+// import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
