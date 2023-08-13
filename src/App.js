f9f6c43257c6e79b5e9adb807bddeda62d5af39c
janusan.lingeswaran <janusan.lingeswaran@rwth-aachen.de>
2023-08-13 18:30:06 +0200

    fix: removed unnecessary margin-bottom to prevent excess scrolling

--- src/App.js
+++ src/App.js
@@ -26,12 +26,12 @@ const Container = styled.div`
 
 const Content = styled.div`
   min-height: calc(100vh - 3.5rem);
-  margin-bottom: -3.5rem;
+  margin-bottom: 0rem;
   z-index: 10;
   filter: ${(props) => (props.isOpen ? 'blur(0.2rem)' : 'none')};
   max-width: 60rem;
   margin: 0 auto;
-  margin-top: 3.4rem;
+  margin-top: 0rem;
 `;
 
 const App = () => {
